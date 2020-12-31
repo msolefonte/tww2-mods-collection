45d7e15f8703c0e439c75dd6771ce6b3f70f9b36
Marc Solé <msolefonte@gmail.com>
2020-12-31 12:29:21 +0100

    Update Extended Multiplayer VCO2 Submod

--- vco2_submod_extended_multiplayer/src/script/vco2_submod_extended_multiplayer/factions/wh2_main_def_har_ganeth/campaigns/main_warhammer/missions.lua
+++ vco2_submod_extended_multiplayer/src/script/vco2_submod_extended_multiplayer/factions/wh2_main_def_har_ganeth/campaigns/main_warhammer/missions.lua
@@ -19,6 +19,27 @@ local missions = {
                 }
             }
         }
+    ]],
+    [[
+        mission
+		{
+			victory_type vco_type_mp_coop;
+			key wh_main_mp_coop_victory;
+			issuer CLAN_ELDERS;
+			primary_objectives_and_payload
+			{
+				objective
+				{
+					type ALL_PLAYERS_RAZE_OR_OWN_X_SETTLEMENTS;
+					total 999;
+				}
+
+				payload
+				{
+					game_victory;
+				}
+			}
+		}
     ]]
 };
 
